d4b79fd453ebf344132eabb0d2f8c847d94635a7
Lewis Barker <l.barker.404@gmail.com>
2015-02-04 10:19:26 +0000

    Reorganised arduino code, added variable power kick

--- Arduino/Receiver/Receiver.ino
+++ Arduino/Receiver/Receiver.ino
@@ -23,91 +23,22 @@ void setup()
   Serial.begin(115200);    // start the serial port at 115200 baud (correct for XinoRF and RFu, if using XRF + Arduino you might need 9600)
   
   Serial.print("STARTED");
-  SCmd.addCommand("ON",LED_on);          // Turns LED on
-  SCmd.addCommand("OFF",LED_off);        // Turns LED off
+  // !!!
+  // Make sure there's no more than ten of these!
+  // Any more and the bottom ones don't work
+  // !!!
   SCmd.addCommand("FWD",wheelsForward);
   SCmd.addCommand("BWD",wheelsBackward);
   SCmd.addCommand("TLEFT",wheelsLeft);
   SCmd.addCommand("TRIGHT",wheelsRight);
   SCmd.addCommand("STOP",wheelsStop);
   SCmd.addCommand("KICK",kick);
-  SCmd.addCommand("GOAL",goalKick);
-  SCmd.addCommand("TESTF",forwardTest);
-  SCmd.addCommand("TESTB",backwardTest);
     
   SCmd.addDefaultHandler(unrecognized);
 }
 void loop()
 {
   SCmd.readSerial();
-}
-
-void f50cm()
-{
-    wheelsForward();
-    delay(2000);
-    wheelsStop();
-}
-void f10cm()
-{
-    wheelsForward();
-    delay(500);
-    wheelsStop();
-}
-void b20cm()
-{
-    wheelsBackward();
-    delay(1000);
-    wheelsStop();
-}
-
-
-void LED_on()
-{
-  Serial.println("LED on"); 
-  digitalWrite(LED_PIN,HIGH);  
-}
-void LED_off()
-{
-  Serial.println("LED off"); 
-  digitalWrite(LED_PIN,LOW);
-}
-void flash(int interval)
-{
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(interval);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(interval);              // wait for a second
-}
-   
-void forwardTest()
-{
-  char *timeStr = SCmd.next();
-  if (timeStr != NULL)
-  {
-    int time = atoi(timeStr);
-    if (time != NULL)
-    {
-      wheelsForward();
-      delay(time);
-      wheelsStop();
-    }
-  }
-}
-    
-void backwardTest()
-{
-  char *timeStr = SCmd.next();
-  if (timeStr != NULL)
-  {
-    int time = atoi(timeStr);
-    if (time != NULL)
-    {
-      wheelsBackward();
-      delay(time);
-      wheelsStop();
-    }
-  }
 }
 
 void wheelsForward()
@@ -138,27 +69,43 @@ void wheelsStop()
 
 void kick()
 {
-  motorForward(4, 40);
-  delay(400);
-  motorStop(4);
-  delay(200);
-  motorBackward(4, 100);
-  delay(300);
-  motorStop(4);
-}
-
-void goalKick()
-{
-  motorForward(4, 60);
-  delay(400);
-  motorStop(4);
-  delay(200);
-  motorBackward(4, 100);
-  delay(300);
-  motorStop(4);
+  char *powerStr = SCmd.next();
+  if (powerStr != NULL)
+  {
+    int power = atoi(powerStr);
+    if (power != NULL)
+    {
+      motorForward(4, power);
+      delay(400);
+      motorStop(4);
+      delay(100);
+      motorBackward(4, power);
+      delay(400);
+      motorStop(4);
+    }
+  }
 }
 
 void unrecognized()
 {
   Serial.println("What?"); 
+}
+
+// LED functions for testing
+void LED_on()
+{
+  Serial.println("LED on"); 
+  digitalWrite(LED_PIN,HIGH);  
+}
+void LED_off()
+{
+  Serial.println("LED off"); 
+  digitalWrite(LED_PIN,LOW);
+}
+void flash(int interval)
+{
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(interval);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(interval);              // wait for a second
 }
